518858606ee204cb5b2bca84bb1ae0582171336d
Ian Wright <49083526+Landaman@users.noreply.github.com>
2025-12-07 16:16:30 -0800

    Add the solution to problem 2 part 2

--- 2025/2/src/main.rs
+++ 2025/2/src/main.rs
@@ -3,7 +3,9 @@ use std::fs;
 fn main() {
     let input = fs::read_to_string("input.txt").expect("Failed to read input file");
 
-    let mut result: i64 = 0;
+    let mut result_one: i64 = 0;
+    let mut result_two: i64 = 0;
+    let regex = pcre2::bytes::Regex::new(r"^(.+)(\g1+)$").unwrap();
 
     input.split(",").for_each(|range| {
         if range.is_empty() {
@@ -19,20 +21,22 @@ fn main() {
         let mut number = lower;
         while number <= upper {
             let number_string = number.to_string();
-            if number_string.len() % 2 != 0 {
-                // skip odd
-                number += 1;
-                continue;
-            }
-            let number_parts = number_string.split_at(number_string.len() / 2);
 
+            let number_parts = number_string.split_at(number_string.len() / 2);
             if number_parts.0 == number_parts.1 {
-                result += number;
+                result_one += number;
+            }
+
+            if regex.is_match(number_string.as_bytes()).unwrap() {
+                result_two += number;
             }
 
             number += 1;
         }
     });
 
-    println!("Sum of invalid IDs: {}", result);
+    println!(
+        "Sum of invalid IDs: {} (part one), {} (part two)",
+        result_one, result_two
+    );
 }
