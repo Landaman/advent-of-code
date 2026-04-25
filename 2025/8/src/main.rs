fc8cafda0e4050a8db312b62c4ebda5878b82599
Ian Wright <49083526+Landaman@users.noreply.github.com>
2026-04-25 18:29:31 -0500

    Solution to day 8, part 2

--- 2025/8/src/main.rs
+++ 2025/8/src/main.rs
@@ -1,4 +1,9 @@
-use std::{cell::RefCell, collections::HashMap, fs, rc::Rc};
+use std::{
+    cell::RefCell,
+    collections::{HashMap, HashSet},
+    fs,
+    rc::Rc,
+};
 
 const DIMENSIONS: usize = 3;
 
@@ -18,62 +23,83 @@ struct Group<'a> {
     points: Vec<&'a Point>,
 }
 
+enum GroupingResult {
+    NewGroup,
+    MergedGroups,
+    AddedToExistingGroup,
+    NoAction,
+}
+
+fn process_edge<'a>(
+    edge: &Edge<'a>,
+    points_to_groups: &mut HashMap<&'a Point, Rc<RefCell<Group<'a>>>>,
+) -> GroupingResult {
+    match (
+        points_to_groups.get(edge.one).cloned(),
+        points_to_groups.get(edge.two).cloned(),
+    ) {
+        (Some(group_one), Some(group_two)) => {
+            if Rc::ptr_eq(&group_one, &group_two) {
+                return GroupingResult::NoAction;
+            }
+
+            let bigger_group_rc: Rc<RefCell<Group>>;
+            let smaller_group_rc: Rc<RefCell<Group>>;
+            if group_one.borrow().size >= group_two.borrow().size {
+                bigger_group_rc = group_one.clone();
+                smaller_group_rc = group_two.clone();
+            } else {
+                bigger_group_rc = group_two.clone();
+                smaller_group_rc = group_one.clone();
+            }
+
+            let mut bigger_group = bigger_group_rc.borrow_mut();
+            let smaller_group = smaller_group_rc.borrow();
+            bigger_group.size += smaller_group.size;
+            for point in &smaller_group.points {
+                bigger_group.points.push(point);
+                points_to_groups.insert(point, bigger_group_rc.clone());
+            }
+
+            // Now, all references to smaller_group should be dropped
+            GroupingResult::MergedGroups
+        }
+
+        (Some(group_one), None) => {
+            points_to_groups.insert(edge.two, group_one.clone());
+            let mut group_one = group_one.borrow_mut();
+            group_one.size += 1;
+            group_one.points.push(edge.two);
+
+            GroupingResult::AddedToExistingGroup
+        }
+
+        (None, Some(group_two)) => {
+            points_to_groups.insert(edge.one, group_two.clone());
+            let mut group_two = group_two.borrow_mut();
+            group_two.size += 1;
+            group_two.points.push(edge.one);
+
+            GroupingResult::AddedToExistingGroup
+        }
+
+        (None, None) => {
+            let group = Rc::new(RefCell::new(Group {
+                size: 2,
+                points: vec![edge.one, edge.two],
+            }));
+            points_to_groups.insert(edge.one, group.clone());
+            points_to_groups.insert(edge.two, group.clone());
+
+            GroupingResult::NewGroup
+        }
+    }
+}
+
 fn part_one(edges: &Vec<Edge>) {
     let mut points_to_groups: HashMap<&Point, Rc<RefCell<Group>>> = HashMap::new();
     for edge in edges.iter().take(CLOSEST_N_EDGES_N) {
-        match (
-            points_to_groups.get(edge.one).cloned(),
-            points_to_groups.get(edge.two).cloned(),
-        ) {
-            (Some(group_one), Some(group_two)) => {
-                if Rc::ptr_eq(&group_one, &group_two) {
-                    continue;
-                }
-
-                let bigger_group_rc: Rc<RefCell<Group>>;
-                let smaller_group_rc: Rc<RefCell<Group>>;
-                if group_one.borrow().size >= group_two.borrow().size {
-                    bigger_group_rc = group_one.clone();
-                    smaller_group_rc = group_two.clone();
-                } else {
-                    bigger_group_rc = group_two.clone();
-                    smaller_group_rc = group_one.clone();
-                }
-
-                let mut bigger_group = bigger_group_rc.borrow_mut();
-                let smaller_group = smaller_group_rc.borrow();
-                bigger_group.size += smaller_group.size;
-                for point in &smaller_group.points {
-                    bigger_group.points.push(point);
-                    points_to_groups.insert(point, bigger_group_rc.clone());
-                }
-
-                // Now, all references to smaller_group should be dropped
-            }
-
-            (Some(group_one), None) => {
-                points_to_groups.insert(edge.two, group_one.clone());
-                let mut group_one = group_one.borrow_mut();
-                group_one.size += 1;
-                group_one.points.push(edge.two);
-            }
-
-            (None, Some(group_two)) => {
-                points_to_groups.insert(edge.one, group_two.clone());
-                let mut group_two = group_two.borrow_mut();
-                group_two.size += 1;
-                group_two.points.push(edge.one);
-            }
-
-            (None, None) => {
-                let group = Rc::new(RefCell::new(Group {
-                    size: 2,
-                    points: vec![edge.one, edge.two],
-                }));
-                points_to_groups.insert(edge.one, group.clone());
-                points_to_groups.insert(edge.two, group.clone());
-            }
-        }
+        process_edge(edge, &mut points_to_groups);
     }
 
     let mut groups: Vec<Rc<RefCell<Group>>> = points_to_groups.values().cloned().collect();
@@ -84,6 +110,36 @@ fn part_one(edges: &Vec<Edge>) {
         .take(LARGEST_N_GROUPS_N)
         .fold(1, |acc, group| acc * group.borrow().size);
     println!("Result: {}", product);
+}
+
+fn part_two(edges: &Vec<Edge>) {
+    let mut unique_points: HashSet<&Point> = edges
+        .iter()
+        .flat_map(|edge| vec![edge.one, edge.two])
+        .collect();
+    let mut points_to_groups: HashMap<&Point, Rc<RefCell<Group>>> = HashMap::new();
+    let mut last_edge: Option<&Edge> = None;
+    let mut num_groups = 0;
+
+    let mut edges_iter = edges.iter();
+    while !unique_points.is_empty() || num_groups > 1 {
+        let edge = edges_iter.next().unwrap();
+        match process_edge(edge, &mut points_to_groups) {
+            GroupingResult::NewGroup => num_groups += 1,
+            GroupingResult::MergedGroups => num_groups -= 1,
+            GroupingResult::AddedToExistingGroup | GroupingResult::NoAction => {}
+        };
+
+        unique_points.remove(edge.one);
+        unique_points.remove(edge.two);
+        last_edge = Some(edge);
+    }
+
+    let checked_last_edge = last_edge.unwrap();
+    println!(
+        "Result: {}",
+        checked_last_edge.one[0] * checked_last_edge.two[0]
+    );
 }
 
 fn main() {
@@ -127,4 +183,5 @@ fn main() {
     }
 
     part_one(&edges);
+    part_two(&edges);
 }
